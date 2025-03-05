12902ada21d019121a36a50af3ef3ccc3ac5d518
ksankita <129387493+ksankita@users.noreply.github.com>
2025-03-05 19:06:03 +0530

    Delete myapp directory

--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from .views import index, check_access
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('check_access/', check_access, name='check_access'),
-]
